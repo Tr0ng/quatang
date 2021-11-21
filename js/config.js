6f92b4b909364928549b121d70495c30f43e2bd1
Tr0ng <89308188+Tr0ng@users.noreply.github.com>
2021-11-21 20:00:16 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'helu chị!',
+    introDesc: `em có điều này muốn thổ lộ với chị`,
+    btnIntro: 'oke',
+    title: 'Chị làm người yêu e nhé <3',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là chị đồng ý đó nha :> ',
+    btnYes: 'chị đồng ý  <3',
+    btnNo: 'e mơ à :3',
+    question: 'và...chị sẽ xưng hô thế nào ',
+    btnReply: 'hmm ??',
+    reply: 'Anh nè, chị yêu em ?',
+    mess: 'Anh biết mà . Yêu chị ',
+    messDesc: 'call cho e nhá.',
+    btnAccept: 'Oki',
+    messLink: 'https://www.facebook.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
